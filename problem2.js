36c0e4ae63c633e51f500e0bc52dd9b7b73ceea4
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-02-05 18:40:37 +0600

    solve problem no-2

--- problem2.js
+++ problem2.js
@@ -2,3 +2,17 @@
  * Find the friend with the smallest name.
 const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
  */
+
+const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+
+function smallestFriend(friends){
+    let smallest = heights2[0];
+    for (const friend of friends) {
+        if (friend.length < smallest.length) {
+            smallest = friend;
+        }
+    }
+    return smallest;
+}
+const output = smallestFriend(heights2);
+console.log(output);
