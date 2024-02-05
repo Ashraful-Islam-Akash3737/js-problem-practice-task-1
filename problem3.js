be121b2a8ac49e755e0997d53e711ce8ba3232ee
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-02-05 18:50:51 +0600

    solve problem no-3

--- problem3.js
+++ problem3.js
@@ -6,3 +6,21 @@
     mobile = 20000 tk
 Write a JavaScript function named calculateElectronicsBudget that takes in the number of laptop, tablets, and mobile and returns the total money required.
  */
+
+const laptop = 35000;
+const tablet = 15000;
+const mobile = 20000;
+
+function totalPrice(totalLaptop, totalTablet, totalMobile){
+    const laptopPrice = totalLaptop * laptop;
+    const tabletPrice = totalTablet * tablet;
+    const mobilePrice = totalMobile * mobile;
+    const totalResult = laptopPrice + tabletPrice + mobilePrice;
+    return totalResult;
+
+}
+const totalLaptop = 2;
+const totalTablet = 3;
+const totalMobile = 5;
+const output = totalPrice(totalLaptop,totalTablet,totalMobile);
+console.log(output);
