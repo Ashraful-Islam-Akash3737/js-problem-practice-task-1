4b5c45ab09694c05f2d750541e4aff5e7d9ac0df
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-02-05 19:00:35 +0600

    solve problem no-4

--- problem4.js
+++ problem4.js
@@ -13,3 +13,21 @@ Input
         { model: "PhoneF", brand: "HTC", price: 48000 },
     ];
  */
+    const phones = [
+        { model: "PhoneA", brand: "Iphone", price: 95000 },
+        { model: "PhoneB", brand: "Samsung", price: 40000 },
+        { model: "PhoneC", brand: "Oppo", price: 26000 },
+        { model: "PhoneD", brand: "Nokia", price: 35000 },
+        { model: "PhoneE", brand: "Iphone", price: 105000 },
+        { model: "PhoneF", brand: "HTC", price: 48000 },
+    ];
+
+    function lowestHeight(phones){
+        let price = 0;
+        for (const phone of phones) {
+            price = price + phone.price;
+        }
+        return price;
+    }
+    const output = lowestHeight(phones);
+    console.log(output);
